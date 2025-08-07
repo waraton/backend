84d6b14e5ef042f6f9a2d262d48299df3eb83e82
waraton <bensonwaran@gmail.com>
2025-08-07 07:14:55 +0300

    login ok

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const express = require('express')
 const myApp = express()
 
 myApp.set("view engine", 'ejs')
+myApp.use(express.urlencoded({extended:false}))
 myApp.use(express.static("public"))
 
 myApp.get('/', (req,res)=>{
-  res.render('home')
+res.render('home')
 })
-
+myApp.get('/login', (req,res)=>{
+res.render('login')
+})
+myApp.post('/register',(req,res)=>{
+  console.log(req.body);
+  res.send('Thaaaaaanks....')
+})
+  
 myApp.listen(3000)
