d67a749caa766debdf65850b24f28b3663fa49b6
waraton <bensonwaran@gmail.com>
2025-08-16 17:24:51 +0300

    db changes

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ myApp.use(function (req, res, next) {
   //Add markdown functionality
   res.locals.filterUserHtml = function (content) {
     return sanitizeHTML(marked.parse(content), {
-      allowedTags: ["p", "li", "table", "em", "strong", "pre", "code"],
+      allowedTags: ["p", "li", "table", "em", "strong", "pre", "code","h2","h3","h4","h5","h6",'blockquote'],
       allowedAttributes: {},
     });
   };
